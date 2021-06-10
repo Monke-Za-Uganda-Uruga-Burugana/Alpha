064c910bf8a4f7c44a8fb04b175cee5c59bf15b9
Trần Đăng Anh <tomdanganh0610@gmail.com>
2021-06-10 16:31:20 +0700

    lol

--- rando-test/main.py
+++ rando-test/main.py
@@ -3,20 +3,20 @@ while True:
   c = "https://api.color.pizza/v1/"
   i = input("Enter hexadecimal value: ")
   try:
-    i1 = int(i, 16) <= 16777215
+   i1 = int(i, 16) <= 16777215
   except:
-    if i == "exit":
-      break
-    else:
-      print("Color not found. Try again.")
+   if i == "exit":
+     break
+   else:
+     print("Color not found. Try again.")
   else:
-    if i1:
-      if len(i) < 6:
-        while len(i) < 6:
-          i = "0" + i
-      res = requests.get(c + i)
-      l = res.text[22:]
-      l2 = l.find("\"")
-      print(l[:l2])
-    else:
-      print("That color was too much for us to handle.")
+   if i1:
+     if len(i) < 6:
+      while len(i) < 6:
+        i = "0" + i
+     res = requests.get(c + i)
+     l = res.text[22:]
+     l2 = l.find("\"")
+     print(l[:l2])
+   else:
+     print("That color was too much for us to handle.")
